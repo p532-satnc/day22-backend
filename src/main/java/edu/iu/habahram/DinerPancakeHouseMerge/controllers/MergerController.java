305e6671eaacbcc47d56e6e772468b78a616d011
N C, Sathya <satnc@iu.edu>
2025-04-14 15:22:11 -0400

    changes

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,5 +1,6 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.CafeRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -20,19 +22,26 @@ import java.util.stream.Stream;
 public class MergerController {
     private final DinerRepository dinerRepository;
     private final PancakeHouseRepository pancakeHouseRepository;
+    private final CafeRepository cafeRepository;
 
     public MergerController(DinerRepository dinerRepository, PancakeHouseRepository pancakeHouseRepository) {
         this.dinerRepository = dinerRepository;
         this.pancakeHouseRepository = pancakeHouseRepository;
+        this.cafeRepository = cafeRepository;
     }
 
     @GetMapping
     public List<MenuItem> getMergedMenu() {
         List<MenuItem> dinerMenu = Arrays.asList(dinerRepository.getTheMenu());
         List<MenuItem> pancakeMenu = pancakeHouseRepository.getTheMenu();
+        List<MenuItem> cafeMenu = (List<MenuItem>) cafeRepository.getTheMenu();
 
-        return Stream.concat(dinerMenu.stream(), pancakeMenu.stream())
-                .sorted(Comparator.comparing(MenuItem::getName))
-                .collect(Collectors.toList());
+        // ✅ Merge all menus in order, no sorting
+        List<MenuItem> mergedMenu = new ArrayList<>();
+        mergedMenu.addAll(dinerMenu);
+        mergedMenu.addAll(pancakeMenu);
+        mergedMenu.addAll(cafeMenu);
+
+        return mergedMenu;
     }
 }
