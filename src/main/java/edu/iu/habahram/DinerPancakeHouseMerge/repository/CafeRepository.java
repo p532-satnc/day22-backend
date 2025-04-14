825f760373c11b4165d1385120a35dbdd542dd71
N C, Sathya <satnc@iu.edu>
2025-04-14 15:19:54 -0400

    changes

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/CafeRepository.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/CafeRepository.java
@@ -1,4 +1,13 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.repository;
 
+import edu.iu.habahram.DinerPancakeHouseMerge.model.CafeMenu;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+
+import java.util.HashMap;
+
 public class CafeRepository {
+    public HashMap<String, MenuItem> getTheMenu() {
+        CafeMenu cafeMenu = new CafeMenu();
+        return (HashMap<String, MenuItem>) cafeMenu.getItems();
+    }
 }
